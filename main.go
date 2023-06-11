b404d29b0ac6c5dab62d1db8faa6017123c4a6c7
ynuraddi <definston@gmail.com>
2023-06-11 08:01:40 +0600

    inited and configured server

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"simple-bank/api"
+	db "simple-bank/db/sqlc"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatalf("cannot connect to db: %v\n", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatalf("cannot start server: %v\n", err)
+	}
+}
