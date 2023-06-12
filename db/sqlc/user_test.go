229f2f08d3738b6ca06e85e1a2d3ba1036de4286
ynuraddi <definston@gmail.com>
2023-06-12 12:40:56 +0600

    fixe db user_test

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
-	hashPassword, err := util.HashedPassword(util.RandomString(6))
+	hashPassword, err := util.HashPassword(util.RandomString(6))
 	require.NoError(t, err)
 
 	arg := CreateUserParams{
