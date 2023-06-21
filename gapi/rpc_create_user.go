6a88f38d36afff8f655a1f114a237e31546e1b79
ynuraddi <definston@gmail.com>
2023-06-21 16:10:52 +0600

    implemented grpc user and login

---
+++ gapi/rpc_create_user.go
@@ -0,0 +1,39 @@
+package gapi
+
+import (
+	"context"
+
+	db "simple-bank/db/sqlc"
+	"simple-bank/pb"
+	"simple-bank/util"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	hashedPassword, err := util.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash pasword: %s", err)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		HashedPassword: hashedPassword,
+		FullName:       req.GetFullName(),
+		Email:          req.GetEmail(),
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if db.ErrorCode(err) == db.UniqueViolation {
+			return nil, status.Errorf(codes.AlreadyExists, "username already exist: %s", err)
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	rsp := &pb.CreateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+}
