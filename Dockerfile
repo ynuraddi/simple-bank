02184c12820107b40635fa7708221d795df242b6
ynuraddi <definston@gmail.com>
2023-06-29 21:05:21 +0600

    feat: migrations now up in golang code

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,8 @@ COPY --from=builder /app/migrate.linux-amd64 ./migrate
 COPY app.env .
 COPY start.sh .
 COPY wait-for.sh .
-COPY db/migration ./migration
+COPY db/migration ./db/migration
 
 EXPOSE 8080
 CMD ["/app/main"]
-ENTRYPOINT ["/app/start.sh"]
+# ENTRYPOINT ["/app/start.sh"]
--- main.go
+++ main.go
@@ -14,6 +14,9 @@ import (
 
 	_ "simple-bank/doc/statik"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/rakyll/statik/fs"
@@ -33,9 +36,23 @@ func main() {
 		log.Fatalf("cannot connect to db: %v\n", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	go runGatewayServer(config, store)
 	runGRPCServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create a new migrate instance:", err)
+	}
+
+	if err = migration.Up(); err != nil {
+		log.Fatal("failed to run migrate up:", err)
+	}
+	log.Println("db migrated successfully")
 }
 
 func runGatewayServer(config util.Config, store db.Store) {
