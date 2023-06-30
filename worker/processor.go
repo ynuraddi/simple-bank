dae81425e4ecd1320fa68556e8e582ddc6a6d2a9
ynuraddi <definston@gmail.com>
2023-07-01 00:47:06 +0600

    feat: adde workers

---
+++ worker/processor.go
@@ -0,0 +1,27 @@
+package worker
+
+import (
+	"context"
+
+	db "simple-bank/db/sqlc"
+
+	"github.com/hibiken/asynq"
+)
+
+type TaskProcessor interface {
+	Start() error
+	ProcessTaskSendVerifyEmail(ctx context.Context, task *asynq.Task) error
+}
+
+type RedisTaskProcessor struct {
+	server *asynq.Server
+	store  db.Store
+}
+
+func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	server := asynq.NewServer(redisOpt, asynq.Config{})
+	return &RedisTaskProcessor{
+		server: server,
+		store:  store,
+	}
+}
